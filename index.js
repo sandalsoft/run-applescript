0ed14d52c9f1ffb7ad4774dfaa6fc8b093439164
Eric Nelson <eric@sandalsoft.com>
2021-03-13 22:42:42 -0500

    fixed import to use require to fix default export upstream in package that imports run-applescript

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import execa from 'execa';
+// Changing this `import` to `require` to fix problem of 'default not exported from execa.  called by run-applescript/index.js'
+// import execa from 'execa';
+const execa = require("execa");
 
 export async function runAppleScriptAsync(script) {
 	if (process.platform !== 'darwin') {
 		throw new Error('macOS only');
 	}
 
-	const {stdout} = await execa('osascript', ['-e', script]);
+	const { stdout } = await execa('osascript', ['-e', script]);
 	return stdout;
 }
 
@@ -14,6 +16,6 @@ export function runAppleScriptSync(script) {
 		throw new Error('macOS only');
 	}
 
-	const {stdout} = execa.sync('osascript', ['-e', script]);
+	const { stdout } = execa.sync('osascript', ['-e', script]);
 	return stdout;
 }
